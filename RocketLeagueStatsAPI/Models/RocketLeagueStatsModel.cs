2a2cb6773134fcbf40016cf5c844c5be32323660
Dylan <dylansprague11@hotmail.com>
2018-11-28 16:00:54 -0500

    added db connection class

---
+++ RocketLeagueStatsAPI/Models/RocketLeagueStatsModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RocketLeagueStatsAPI.Models
+{
+    public class RocketLeagueStatsModel : DbContext
+    {
+        public RocketLeagueStatsModel(DbContextOptions<RocketLeagueStatsModel>options): base(options)
+        {
+
+        }
+    }
+}
