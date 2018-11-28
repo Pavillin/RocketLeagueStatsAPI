97c4411577e1af8bcfde0efdbd98fbf3ffd7ee03
Dylan <dylansprague11@hotmail.com>
2018-11-28 16:11:07 -0500

    added team model

---
+++ RocketLeagueStatsAPI/Models/team.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RocketLeagueStatsAPI.Models
+{
+    [Table("teams")]
+    public class team
+    {
+        public int teamid { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string name { get; set; }
+
+        [Required]
+        [StringLength(3)]
+        public string region { get; set; }
+
+        public int wins { get; set; }
+
+        public int losses { get; set; }
+    }
+}
